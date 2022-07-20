81ff444ac9eba93c6121a8e2a44f2355461bfa9c
jejung <jejung@growthsoft.co.kr>
2022-07-20 18:00:36 +0900

    Added # 파일 업로드 후 파싱 문구 추가

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,28 @@
 export default function Home () {
-  return '안녕, 프로젝트 리셋했어'
+  const changeEvent = (e) => {
+    const file = e.target.files[0]
+
+    const reader = new FileReader()
+    reader.readAsText(file)
+    reader.onload = () => {
+      // 콜백지옥 빠져나가기 ㄷㄷ
+      const split = data => data.split(',')
+
+      // const raw = reader.result.split('\n')
+      const raw = reader.result
+      const enter = raw.indexOf('\n')
+      const columns = split(raw.slice(0, enter))
+      const test = raw.slice(enter)
+      // const columns = split(raw[0])
+
+
+      // console.log(reader.result.split('\n'))
+      console.log(columns)
+      console.log(test)
+      // columns.split
+
+    }
+  }
+
+  return <input type="file" onChange={changeEvent}/>
 }
