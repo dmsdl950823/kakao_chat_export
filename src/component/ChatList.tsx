ec2937783a6c201d68f2b7eb805c236753f016e2
정재은 <dmsdl950823@gmail.com>
2022-08-01 18:01:02 +0900

    Updated #0801 채팅 - 나 확인

--- src/component/ChatList.tsx
+++ src/component/ChatList.tsx
@@ -2,31 +2,39 @@ import React from 'react'
 import '../style/chartlist.css'
 
 type AppProps = {
-  value: string
+  name: string
+  content: string
+  me: boolean
 }
 
-export default function ChatList ({ value }: AppProps) {
+export default function ChatList ({ name, content, me }: AppProps) {
   const setStyle = () => {
     return { height: '200px' }
   }
 
-  const textarea = (
-    <div className="-chat">
-      <textarea
-        style={ setStyle() }
-        value={ value }
-        readOnly
-      />
-    </div>
-  )
+  // 채팅이 나인지 확인
+  const setListType = (isMe: boolean) => {
+    return {
+      className: isMe ? '-list -me' : '-list',
+      name: isMe ? null : <span className="name">{name}</span>,
+      image: isMe ? null : <div className="-image"></div>
+    }
+  }
 
   return (
-    <li className="-list">
-      <div className="-image"></div>
+    <li className={setListType(me).className}>
+        { setListType(me).image }
 
       <div className="-profile">
-        <span className="name">Name</span>
-        { textarea }
+        { setListType(me).name }
+
+        <div className="-chat">
+          <textarea
+            style={ setStyle() }
+            value={ content }
+            readOnly
+          />
+        </div>
       </div>
     </li>
   )
--- src/view/Home.js
+++ src/view/Home.js
@@ -3,15 +3,26 @@ import ChatList from '../component/ChatList'
 // import Input from '../component/Input'
 
 export default function Home () {
-  const numbers = [1, 2, 3, 4, 5]
-  const listItems = numbers.map((number) => {
+  const numbers = [
+    { name: 'AD', content: '테스트테스트테스트', id: 0 },
+    { name: 'AD', content: '테스트테스트테스트', id: 1 },
+    { name: '또야', content: '테스트테스트테스트', id: 2, me: true },
+    { name: 'AD', content: '테스트테스트테스트', id: 3 },
+    { name: '또야', content: '테스트테스트테스트', id: 4, me: true },
+    { name: 'AD', content: '테스트테스트테스트', id: 5 }
+  ]
+
+  const listItems = numbers.map(({ name, content, me, id }) => {
     return (
       <ChatList
-        key={number.toString()}
-        value="안녕하세요?\n\n안녕하세요"
+        key={id}
+        me={me}
+        name={name}
+        value={content}
       />
     )
   })
+
   return (
     <div>
       <ul className="chart-list">
